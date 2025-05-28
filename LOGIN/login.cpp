d8efeb9bd5df468f203ed3de4fbc156ebe53c5b6
martin <martindaparoo12@gmail.com>
2025-05-28 13:09:46 -0300

    login progreso 1

--- LOGIN/login.cpp
+++ LOGIN/login.cpp
@@ -4,3 +4,12 @@ using namespace std;
 void mostrarLogin() {
     cout << "Hola desde login!" << endl;
 }
+void login() {
+    cout << "Iniciar sesion." << endl;
+
+    cout << "Por favor, ingrese sus credenciales." << endl;
+    cout << "Usuario: ";
+    cout << "Usuario: admin" << endl;
+    cout << "Contraseña: admin" << endl;
+}
+
