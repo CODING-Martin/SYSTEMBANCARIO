12d46b608040fcaff7c3f40586d982341c0eacc2
martin <martindaparoo12@gmail.com>
2025-05-27 23:53:26 -0300

    compilador del proyecto general

---
+++ compilar.bat
@@ -0,0 +1,14 @@
+[22:38, 27/5/2025] Martin: ${workspaceFolder}/INTERFACE
+[23:25, 27/5/2025] Martin: @echo off
+echo Compilando sistema bancario...
+
+g++ main.cpp LOGIN\login.cpp USERS\gestionUsuarios.cpp OPERATIONS\operaciones.cpp INTERFACE\interfaz.cpp -o BIN\bancario.exe
+
+if %errorlevel% neq 0 (
+    echo Error en la compilacion.
+) else (
+    echo Compilacion exitosa. Ejecutando...
+    BIN\bancario.exe
+)
+
+pause
