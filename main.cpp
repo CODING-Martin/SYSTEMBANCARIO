cbb5048226b6cd3f6a33970daa3d4c88d3f7488c
pedroA37 <pedroayala2812@gmail.com>
2025-05-29 00:09:04 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,19 +4,21 @@ using namespace std;
 #include "OPERATIONS/operaciones.h"
 #include "INTERFACE/interfaz.h"
 #include "USERS/gestionUsuarios.h"
+#include <string>
 
-
-
-
-void registroLogin(); 
+void mostrarLogin();
+void mostrarOperaciones();
+void mostrarInterface();
+void mostrarGestionUsuarios();
+void login();
+void interfaz();
 
 
 int main()
 {
     cout << "Bienvenido al sistema bancario." << endl;
-    cout << "Seleccione idioma / Select language (es/en): ";
-    cin >> idioma;
-    registroLogin(); // Llamada a la función de registro de inicio de sesión
-
+    mostrarLogin();
+    login();
+    interfaz();
     return 0;
 }
