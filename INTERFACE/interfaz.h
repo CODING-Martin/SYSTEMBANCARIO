1c5e15098dd5fa5c93d2d9c784397c6f772e6f39
pedroa37 <pedroayala2812@gmail.com>
2025-05-30 00:24:50 -0300

    Correccion de direcciones

--- INTERFACE/interfaz.h
+++ INTERFACE/interfaz.h
@@ -1,8 +1,11 @@
 #ifndef interfaz_h
 #define interfaz_h
 #include <string>
+#include "USERS/gestionUsuarios.h"
 using namespace std;
-
-void interfaz();
+//codigo sub main
+void interfaz(string username);
+//codigos complementarios
+void opcionesMenu(int opcion);
 
 #endif
