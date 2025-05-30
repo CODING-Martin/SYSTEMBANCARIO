144c18788b62221f8f3c16903d25e8ad3fbe98a4
pedroa37 <pedroayala2812@gmail.com>
2025-05-30 01:42:47 -0300

    Adicion de detalles

--- INTERFACE/mensajeError.cpp
+++ INTERFACE/mensajeError.cpp
@@ -4,6 +4,9 @@ using namespace std;
 
 void mensajeDeError(int opcion, int limite){
     cout<<"La opcion "<<opcion<<" no existe. Revise los datos ingresados e Intente Nuevamente.\n";
+    cout<<endl;
+    cout<<endl;
+    cout << "Pulse ENTER para continuar...";
     cin.get();
 }
 
