b44ed7325c66ed2bb8c120e96bf8a71425f16f3a
pedroa37 <pedroayala2812@gmail.com>
2025-05-30 01:38:03 -0300

    Cerrar sesion

---
+++ OPERATIONS/cerrarSesion.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "interfaz.h"
+#include <unistd.h>
+using namespace std;
+
+void cerrarSesion(){
+    char confirmar;
+    int i; 
+    cout<<"¿Esta seguro que desea cerrar sesion? (s/n): ";
+    cin >> confirmar;
+    if(confirmar == 's' || confirmar == 'S'){
+        cout << "Cerrando sesion";
+        for(i = 3; i > 0; --i){
+            sleep(1000);
+            cout <<".";
+    }
+}
