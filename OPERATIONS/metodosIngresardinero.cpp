757144174d99ec61db933adb51bc537181042e65
pedroa37 <pedroayala2812@gmail.com>
2025-05-31 00:18:12 -0300

    fixeo de errores

--- OPERATIONS/metodosIngresardinero.cpp
+++ OPERATIONS/metodosIngresardinero.cpp
@@ -18,6 +18,10 @@ void metodosIngresoDinero(double& saldo, int opcion) {
                 cout << "Ingrese el importe a ingresar: $";
                 cin >> dinero;
                 if (dinero <= 0) {
+                    system("cls");
+                    cout << "====================================" << endl;
+                    cout << "          INGRESO DE DINERO" << endl;
+                    cout << "====================================" << endl;
                     cout << "El importe debe ser mayor a 0. Intente nuevamente." << endl;
                     cout << "Presione ENTER para continuar...";
                     cout << "====================================" << endl;
@@ -32,8 +36,8 @@ void metodosIngresoDinero(double& saldo, int opcion) {
             cout << "          INGRESO DE DINERO" << endl;
             cout << "====================================" << endl;
             cout << endl;
-            cout << "Su ingreso fue exitoso. Su saldo ahora es de $" << fixed << setprecision(2) << saldo << "." << endl;
-            cout << endl;
+            cout << "Su ingreso fue exitoso."<<endl; 
+            cout << "Su saldo ahora es de $" << fixed << setprecision(2) << saldo << "." << endl;
             cout << "Presione ENTER para continuar..."<<endl;
             cout << "====================================" << endl;
             cin.ignore(numeric_limits<streamsize>::max(), '\n');
@@ -51,6 +55,9 @@ void metodosIngresoDinero(double& saldo, int opcion) {
                 cout << "====================================" << endl;
                 if (dinero <= 0) {
                     system("cls");
+                    cout << "====================================" << endl;
+                    cout << "          INGRESO DE DINERO" << endl;
+                    cout << "====================================" << endl;
                     cout << "El importe debe ser mayor a 0. Intente nuevamente." << endl;
                     cout << "Presione ENTER para continuar..."<<endl;
                     cout << "====================================" << endl;
@@ -64,7 +71,8 @@ void metodosIngresoDinero(double& saldo, int opcion) {
             cout << "====================================" << endl;
             cout << "          INGRESO DE DINERO" << endl;
             cout << "====================================" << endl;
-            cout << "Su ingreso fue exitoso. Su saldo ahora es de $" << fixed << setprecision(2) << saldo << "." << endl;
+            cout << "Su ingreso fue exitoso."<<endl; 
+            cout << "Su saldo ahora es de $" << fixed << setprecision(2) << saldo << "." << endl;
             cout << endl;
             cout << "Presione ENTER para continuar..."<<endl;
             cout << "====================================" << endl;
